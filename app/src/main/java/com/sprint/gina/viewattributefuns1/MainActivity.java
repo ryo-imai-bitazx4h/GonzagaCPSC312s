4dca42be9efe048b73d275c1281b6e9dfe34dc26
ryo-imai-bitazx4h <jiaotaozhuo3@126.com>
2021-03-20 13:16:04 +0800

    initial

---
+++ app/src/main/java/com/sprint/gina/viewattributefuns1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.sprint.gina.viewattributefuns1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*
+        Add an event handler to your button so whenever the user clicks,
+        the button’s text switches to “On” if it was “Off” and “Off” if it was “On”.
+         */
+    }
+}
