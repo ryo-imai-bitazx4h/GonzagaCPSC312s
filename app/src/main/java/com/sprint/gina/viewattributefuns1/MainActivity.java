2532985b334fb36c63f5fe7f19b5277f43239667
ryo-imai-bitazx4h <jiaotaozhuo3@126.com>
2021-03-22 11:56:24 +0800

    end of attributes

--- app/src/main/java/com/sprint/gina/viewattributefuns1/MainActivity.java
+++ app/src/main/java/com/sprint/gina/viewattributefuns1/MainActivity.java
@@ -3,6 +3,8 @@ package com.sprint.gina.viewattributefuns1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
         /*
         Add an event handler to your button so whenever the user clicks,
         the button’s text switches to “On” if it was “Off” and “Off” if it was “On”.
-         */
+        */
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String onText = getString(R.string.on);
+                String offText = getString(R.string.off);
+                if (button.getText().toString().compareTo(onText) == 0) {
+                    button.setText(offText);
+                }
+                else {
+                    button.setText(onText);
+                }
+            }
+        });
     }
 }
